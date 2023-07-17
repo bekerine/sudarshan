bc268b9a5e06be79da8f681e73d3915713b88275
bekerine <95853902+bekerine@users.noreply.github.com>
2023-07-17 23:31:21 +0530

    Update script.js

--- Trial Animations/Vowels/script.js
+++ Trial Animations/Vowels/script.js
@@ -1,45 +1,45 @@
 function checkAnswers() {
-    var answers = {
-      q1: {
-        correctAnswers: ['a','e','i','o','u'],
-        minOptions: 5
-      },
-      // Add more questions here...
-    };
-  
-    var resultContainer = document.getElementById('result');
-    resultContainer.textContent = '';
-  
-    var allCorrect = true;
-  
-    for (var question in answers) {
-      var correctAnswers = answers[question].correctAnswers;
-      var minOptions = answers[question].minOptions;
-  
-      var selectedAnswers = [];
-      var checkboxes = document.getElementsByName(question);
-  
-      for (var i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-          selectedAnswers.push(checkboxes[i].value);
-        }
-      }
-  
-      if (selectedAnswers.length < minOptions) {
-        allCorrect = false;
-        resultContainer.innerHTML += '<p>Please select at least ' + minOptions + ' option(s) for question ' + question.substr(1) + '.</p>';
-      } else {
-        for (var i = 0; i < selectedAnswers.length; i++) {
-          if (!correctAnswers.includes(selectedAnswers[i])) {
-            allCorrect = false;
-            resultContainer.innerHTML += '<p>Incorrect answer for question ' + question.substr(1) + '.</p>';
-            break;
-          }
-        }
+  var answers = {
+    q1: {
+      correctAnswers: ['a','e','i','o','u'],
+      minOptions: 5
+    },
+    // Add more questions here...
+  };
+
+  var resultContainer = document.getElementById('result');
+  resultContainer.textContent = '';
+
+  var allCorrect = true;
+
+  for (var question in answers) {
+    var correctAnswers = answers[question].correctAnswers;
+    var minOptions = answers[question].minOptions;
+
+    var selectedAnswers = [];
+    var checkboxes = document.getElementsByName(question);
+
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        selectedAnswers.push(checkboxes[i].value);
       }
     }
-  
-    if (allCorrect) {
-      resultContainer.innerHTML += '<p>All answers are correct!</p>';
+
+    if (selectedAnswers.length < minOptions) {
+      allCorrect = false;
+      resultContainer.innerHTML += '<p>Please select at least ' + minOptions + ' option(s) for question ' + question.substr(1) + '.</p>';
+    } else {
+      for (var i = 0; i < selectedAnswers.length; i++) {
+        if (!correctAnswers.includes(selectedAnswers[i])) {
+          allCorrect = false;
+          resultContainer.innerHTML += '<p>Incorrect answer for question ' + question.substr(1) + '.</p> <a href="#"><button>get explanation</button></a>';
+          break;
+        }
+      }
     }
   }
+
+  if (allCorrect) {
+    resultContainer.innerHTML += '<p>All answers are correct!</p>';
+  }
+}
