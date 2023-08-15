23dc0543e137f79d603244b550f9ce184371dbf8
KnightofInd <126257520+KnightofInd@users.noreply.github.com>
2023-08-15 18:36:16 +0530

    Update pi2+x.js
    EKLAVYA

--- Interactive NCERT/Frontend/Modules/pi2+x.js
+++ Interactive NCERT/Frontend/Modules/pi2+x.js
@@ -8,25 +8,42 @@ let activeCircle = null;
 let questionNumber = 1;
 
 function setup() {
-  createCanvas(400, 250);
+  createCanvas(700, 400);
 
-  circle1 = new Circle(100, 150, 50, PI*0.835); // The first circle's radius is set to 150 degrees (PI/2 radians)
-  circle2 = new Circle(300, 150, 50, 0, HALF_PI); // The second circle's radius starts at 0 degrees (HALF_PI radians)
-
+  circle1 = new Circle(200, 200, 100, -PI*0.834); // The first circle's radius is set to 150 degrees (PI/2 radians)
+  circle2 = new Circle(500, 200, 100,0, TWO_PI); // The second circle's radius starts at 0 degrees (HALF_PI radians)
 
   submitButton = createButton("Submit");
-  submitButton.position(40, 10);
+  submitButton.position(290, 50);
   submitButton.mousePressed(checkCondition);
+  submitButton.style('font-size', '24px');
+  submitButton.style('padding', '5px 20px');
+  submitButton.style('border', '');
+  submitButton.style('background-color', 'blue');
+  submitButton.style('color', 'white');
+  submitButton.style('border-radius', '10px');
 
   retryButton = createButton("Retry");
-  retryButton.position(width / 2 - 40, height - 40);
+  retryButton.position(width / 2 - 50, height -90);
   retryButton.mousePressed(resetFigure);
   retryButton.hide();
+   retryButton.style('font-size', '24px');
+  retryButton.style('padding', '5px 20px');
+  retryButton.style('border', '');
+  retryButton.style('background-color', 'red');
+  retryButton.style('color', 'white');
+  retryButton.style('border-radius', '10px');
 
   nextButton = createButton("Next Question");
-  nextButton.position(width / 2 - 45, height / 2 + 50);
+  nextButton.position(width / 2-100, height / 2 + 100);
   nextButton.mousePressed(nextQuestion);
   nextButton.hide();
+  nextButton.style('font-size', '24px');
+  nextButton.style('padding', '5px 20px');
+  nextButton.style('border', '');
+  nextButton.style('background-color', 'green');
+  nextButton.style('color', 'white');
+  nextButton.style('border-radius', '10px');
 }
 
 function resetFigure() {
@@ -38,7 +55,7 @@ function resetFigure() {
 }
 
 function checkCondition() {
-  if (round(degrees(circle2.angle)) === -60) {
+  if (round(degrees(circle2.angle)) === 60) {
     successMessage = true;
     retryButton.hide();
     nextButton.show();
@@ -106,7 +123,7 @@ class Circle {
     // Display the angle as text
     noStroke();
     fill(0);
-    text(round(degrees(this.angle)) + "°", this.x + this.r + 10, this.y + 5);
+    text(round(degrees(-this.angle)) + "°", this.x + this.r + 10, this.y + 5);
   }
 }
 
@@ -136,4 +153,3 @@ function draw() {
     text("Successful", width / 2, height - 30);
   }
 }
-
