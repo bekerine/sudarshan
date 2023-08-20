6cb4a6adfa3ae02de83197df37805369b0db71bd
bekerine <95853902+bekerine@users.noreply.github.com>
2023-08-20 10:43:16 +0530

    g

--- website/main-page/sidebar-09/js/index.js
+++ website/main-page/sidebar-09/js/index.js
@@ -1,4 +1,4 @@
-// Get the button
+// Get the button start
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,3 +16,19 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+// top button end
+
+// 5s pop-up start
+    // Function to display the pop-up after 5 seconds
+    function showPopup() {
+      document.getElementById('popup').classList.add('active');
+    }
+
+    // Function to close the pop-up
+    function closePopup() {
+      document.getElementById('popup').classList.remove('active');
+    }
+
+    // Call the showPopup function after 5 seconds
+    setTimeout(showPopup, 5000); // 5000 milliseconds (5 seconds)
+// 5s pop-up bend
