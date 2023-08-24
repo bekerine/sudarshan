1ff4b3b472c61b1c187657b909842e640f21adfc
bekerine <95853902+bekerine@users.noreply.github.com>
2023-08-24 11:13:13 +0530

    g

--- website/New folder/iframe/index.js
+++ website/New folder/iframe/index.js
@@ -16,3 +16,18 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// 5s pop-up start
+    // Function to display the pop-up after 5 seconds
+    function showPopup() {
+      document.getElementById('popup').classList.add('active');
+    }
+
+    // Function to close the pop-up
+    function closePopup() {
+      document.getElementById('popup').classList.remove('active');
+    }
+
+    // Call the showPopup function after 5 seconds
+    setTimeout(showPopup, 5000); // 5000 milliseconds (5 seconds)
+// 5s pop-up bend
